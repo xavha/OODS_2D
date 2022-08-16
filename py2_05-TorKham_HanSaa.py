878ff945f9a6679526198f8198fa6b1edacb5947
Xavha <xavhaswha@gmail.com>
2022-08-17 00:41:08 +0700

    python1,2

--- py2_05-TorKham_HanSaa.py
+++ py2_05-TorKham_HanSaa.py
@@ -1,75 +1,35 @@
-# def torkham(word,newgame) :
-#     print(newgame)
-#     if word[0] == 'P' :
-#         if newgame:
-#             out=[]
-#             out.append(word[1])
-#             check=word[1][-2]+word[1][-1]
-#             print(out)
-#             newgame=False
-#         else :
-#             if word[1][0]+word[1][1] == check :
-#                 out.append(word[1])
-#                 print(out)
-#     elif word[0] == 'R' :
-#         out=[]
-#         newgame=True
-#         print("game restarted")
-#     elif word[0] == 'X' :
-#         exit()
-#     else :
-#         print(" is Invalid Input !!!")
-#         exit()
-
-wordlist = []
-check = ''
-
-class torkham :
+class torkham:
     def __init__(self):
-        self.newgame = True
-        pass
-    def next(self,word) :
-        global wordlist,check
-        self.newgame = newgame
-        self.word = word
-        if self.newgame:
-            wordlist.append(word)
-            check=word[-2]+word[-1]
-            print(wordlist)
-            self.newgame=False
-        else :
-            if word[1][0]+word[1][1] == check :
-                wordlist.append(word[1])
-                print(wordlist)
-        pass
-    def restart():
-        global wordlist,newgame
-        newgame=True
-        wordlist=[]
-        print("game restarted")
-        pass
-        
+        self.wordList = []
 
+    def next(self, word):
+        if self.wordList == []:
+            self.wordList.append(word)
+            print('\''+str(word)+'\' ->',self.wordList)
+        else:
+            if self.wordList[len(self.wordList)-1][-1].casefold()==word[1].casefold() and self.wordList[len(self.wordList)-1][-2].casefold()==word[0].casefold():
+                self.wordList.append(word)
+                print('\''+str(word)+'\' ->',self.wordList)
+            else:
+                print('\''+str(word)+'\' -> game over')
     
+    def restart(self):
+        self.wordList = []
+        print("game restarted")
+
 
 print("*** TorKham HanSaa ***")
-a = input("Enter Input : ").split(",")
+a = list(map(str,input("Enter Input : ").split(",")))
+Torkham=torkham()
 
-
-
-for i in range (len(a)) :
-    word=a[i].split(" ")
+for i in a:
+    word = i.split()
     if word[0] == 'P' :
-        torkham.next(word[1])
+        Torkham.next(word[1])
     elif word[0] == 'R' :
-        torkham.restart()
+        Torkham.restart()
     elif word[0] == 'X' :
         exit()
     else :
-        print(" is Invalid Input !!!")
-    
-    
-    # torkham(word,newgame)
-    
-
-       
+        print('\''+' '.join(word)+'\' is Invalid Input !!!')
+        break
